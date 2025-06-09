23fe5a6fe62da200a9d68bfbcdaf8056afe0ebce
Jakub Daněk <114767868+kuboous@users.noreply.github.com>
2025-06-09 22:42:57 +0200

    Delete skript.js

--- skript.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelectorAll('.tlacitko');
-
-btn.forEach(function(tlacitko) {
-    tlacitko.addEventListener('click', function(){
-        //pro každou dočasnou proměnnou tlacitko se nám zastaví event listener onClick
-        alert('Kliknul jste na tlačítko!');
-    })
-});
